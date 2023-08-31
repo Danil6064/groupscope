c1d6382c3f19946e75ae688b3efbaddcd1b09c04
Danil Nikonenko <danilnikonenko04@gmail.com>
2023-08-31 11:57:58 +0300

    Global update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom/client';
-=======
-import ReactDOM from 'react-dom';
->>>>>>> 9c66d1cf54a1d5acc5699260c428742d2f684960
 import reportWebVitals from './reportWebVitals';
 import './main.css';
 
@@ -23,7 +19,6 @@ const rootElement = document.getElementById('root');
 
 const root = ReactDOM.createRoot(rootElement);
 
-<<<<<<< HEAD
 root.render(
   <BrowserRouter>
     <AuthProvider>
@@ -39,10 +34,10 @@ root.render(
             </PrivateRoute>
           }/>
 
-          <Route path="/subject/:subjectName" element={
-<PrivateRoute roles={['HEADMAN', 'STUDENT']}>
-            <TaskPage />
-          </PrivateRoute>
+          <Route path="/subject/:id" element={
+            <PrivateRoute roles={['HEADMAN', 'STUDENT']}>
+              <TaskPage />
+            </PrivateRoute>
           }/>
 
           <Route path="/successfulStudent" element={
@@ -59,127 +54,7 @@ root.render(
         </Routes>
       </StudentProvider>
     </AuthProvider>
-=======
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home';
-import Header from './components/header/Header';
-import { createRoot } from 'react-dom/client';
-import TaskPage from './pages/tasksPage/TaskPage';
-import SuccessfulStunent from './pages/successfulStudent/SuccessfulStudent';
-import SuccessfulGroup from './pages/successfulGroup/SuccessfulGroup';
-
-const rootElement = document.getElementById('root');
-
-createRoot(rootElement).render(
-  <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/subject/:id" element={<TaskPage />}/>
-      <Route path="/successfulStudent" element={<SuccessfulStunent />}/>
-      <Route path="/successfulGroup" element={<SuccessfulGroup />}/>
-    </Routes>
->>>>>>> 9c66d1cf54a1d5acc5699260c428742d2f684960
   </BrowserRouter>
 );
 
 reportWebVitals();
-
-
-
-<<<<<<< HEAD
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import reportWebVitals from './reportWebVitals';
-// import './main.css';
-
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Auth from './pages/auth/Auth';
-// import Guest from './pages/guest/Guest';
-// import Home from './pages/home/Home';
-
-// import { AuthProvider } from './pages/auth/AuthContext'; // Припустимий шлях до вашого файлу AuthContext
-// import Header from './components/header/Header';
-// import { createRoot } from 'react-dom/client';
-// import TaskPage from './pages/tasksPage/TaskPage';
-// import SuccessfulStudent from './pages/successfulStudent/SuccessfulStudent';
-// import SuccessfulGroup from './pages/successfulGroup/SuccessfulGroup';
-
-// import { StudentProvider } from '../src/helpers/StudentContext';
-
-// const rootElement = document.getElementById('root');
-
-// createRoot(rootElement).render(
-//   <BrowserRouter>
-//     <Header />
-//     <StudentProvider>
-//       <AuthProvider> {/*ми додали AuthProvider */}
-//         <Routes>
-//           <Route path='/auth' element={<Auth />} />
-//           <Route path='/guest' element={<Guest />} />
-//           <Route path="/" element={<Home />} />
-//           <Route path="/subject/:id" element={<TaskPage />} />
-//           <Route path="/successfulStudent" element={<SuccessfulStudent />} />
-//           <Route path="/successfulGroup" element={<SuccessfulGroup />} />
-//         </Routes>
-//       </AuthProvider>
-//     </StudentProvider>
-//   </BrowserRouter>,
-// );
-
-
-// reportWebVitals();
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import reportWebVitals from './reportWebVitals';
-// import './main.css'
-
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Auth from './pages/auth/Auth';
-// import Quest from './pages/guest/Quest';
-// import Home from './pages/home/Home';
-
-// import Header from './components/header/Header';
-// import { createRoot } from 'react-dom/client';
-// import TaskPage from './pages/tasksPage/TaskPage';
-// import SuccessfulStunent from './pages/successfulStudent/SuccessfulStudent';
-// import SuccessfulGroup from './pages/successfulGroup/SuccessfulGroup';
-
-// const rootElement = document.getElementById('root');
-
-// createRoot(rootElement).render(
-//   <BrowserRouter>
-//     <Header />
-//     <Routes>
-//       <Route path='/auth' element={<Auth />}/>
-//       <Route path='/quest' element={<Quest/>}/>
-//       <Route path="/" element={<Home />}/>
-//       <Route path="/subject/:id" element={<TaskPage />}/>
-//       <Route path="/successfulStudent" element={<SuccessfulStunent />}/>
-//       <Route path="/successfulGroup" element={<SuccessfulGroup />}/>
-//     </Routes>
-//   </BrowserRouter>
-// );
-
-// reportWebVitals();
-
-
-
-=======
->>>>>>> 9c66d1cf54a1d5acc5699260c428742d2f684960
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <Header />
-//     <Home />
-//   </React.StrictMode>
-// );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
--- src/pages/successfulStudent/SuccessfulStudent.js
+++ src/pages/successfulStudent/SuccessfulStudent.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import './successStudent.css';
 import ChoseSubjectMenu from '../../components/choseSubject/ChoseSubjectMenu';
@@ -20,26 +19,3 @@ function SuccessfulStunent() {
 }
 
 export default SuccessfulStunent;
-=======
-import React, { useState } from 'react';
-import './successStudent.css';
-import ChoseSubjectMenu from '../../components/choseSubject/ChoseSubjectMenu';
-import TaskCardSuccessful from '../../components/taskCard/taskCardSuccessful/TaskCardSuccessful';
-
-function SuccessfulStunent() {
-  const [selectedSubject, setSelectedSubject] = useState(null);
-
-  return (
-    <main className="main_successfulStudent">
-      <div className="container_successfulStudent">
-        <ChoseSubjectMenu setSelectedSubject={setSelectedSubject} />
-        {selectedSubject && (
-          <TaskCardSuccessful selectedSubject={selectedSubject} />
-        )}
-      </div>
-    </main>
-  );
-}
-
-export default SuccessfulStunent;
->>>>>>> 9c66d1cf54a1d5acc5699260c428742d2f684960
