24609faa9415540b3b6dac6e363b8a11b3b27c68
TimeSlayer <nikitajudo313@gmail.com>
2023-10-05 00:04:22 +0300

    Added homework task info button, improved homework tasks adaptation

--- src/components/taskCard/TaskCard.js
+++ src/components/taskCard/TaskCard.js
@@ -1,18 +1,35 @@
-import React from 'react';
-import './taskCard.css'
+import React, { useState } from "react";
+import "./taskCard.css";
 
-const TaskCard = ({ name, info, deadline }) => {
+export default function TaskCard({ name, info, deadline }) {
+  const [showMore, setShowMore] = useState(false);
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
   return (
     <li className="homework-card">
       <h3>{name}</h3>
-      <div className="homework-text">
-        <span>{info}</span>
+      <div className="task-info">
+        <button className="task-info-btn" onClick={handleMoreClick}>
+          <svg width="10" height="10" viewBox="0 0 19 19">
+            <polygon points="0,0 19,0 9.5,19" />
+          </svg>
+          <span>{showMore ? "згорнути" : "розгорнути"} опис завдання</span>
+          <svg width="10" height="10" viewBox="0 0 19 19">
+            <polygon points="0,0 19,0 9.5,19" />
+          </svg>
+        </button>
       </div>
+      {showMore && (
+        <div className="homework-text">
+          <span>{info}</span>
+        </div>
+      )}
       <div className="homework-deadline">
         <span>Deadline: {deadline}</span>
       </div>
     </li>
   );
-};
-
-export default TaskCard;
+}
