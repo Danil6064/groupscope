256c54f748759022dbb1edb3ac90325ce59193a1
TimeSlayer <nikitajudo313@gmail.com>
2023-10-21 00:21:50 +0300

    All requests have been finalized in advance

--- src/helpers/MainConstants.js
+++
@@ -1,3 +0,0 @@
-export const apiUrl = "https://localhost:8443/api"
-
-export const url = "https://localhost:8443"
--- src/pages/guest/Guest.js
+++ src/pages/guest/Guest.js
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import "./guest.css";
 import { useNavigate } from "react-router-dom";
-import { apiUrl } from "../../helpers/MainConstants";
-import useAuth from "../../hooks/useAuth";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
-function Guest() {
+export default function Guest() {
   const [action, setAction] = useState(null);
   const [groupName, setGroupName] = useState("");
   const [inviteCode, setInviteCode] = useState("");
@@ -17,48 +15,61 @@ function Guest() {
   const handleCreateGroup = async () => {
     axiosPrivate
       .post("api/group/create", { name: groupName })
-      .then(navigate("/"))
-      .catch((error) => console.error(error));
+      .then(() => {
+        navigate("/home");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
-    const requestBody = {
-      name: groupName,
-    };
+    //   const requestBody = {
+    //     name: groupName,
+    //   };
 
-    const response = await fetch(`${apiUrl}/group/create`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + jwtToken,
-      },
-      body: JSON.stringify(requestBody),
-    });
+    //   const response = await fetch(`${apiUrl}/group/create`, {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: "Bearer " + jwtToken,
+    //     },
+    //     body: JSON.stringify(requestBody),
+    //   });
 
-    if (response.ok) {
-      navigate("/auth");
-    } else {
-      // Handle the error
-    }
+    //   if (response.ok) {
+    //     navigate("/auth");
+    //   } else {
+    //     // Handle the error
+    //   }
   };
 
   const handleJoinGroup = async () => {
-    const requestBody = {
-      inviteCode: inviteCode,
-    };
+    axiosPrivate
+      .post("api/group/join", { inviteCode: inviteCode })
+      .then(() => {
+        navigate("/home");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
-    const response = await fetch(`${apiUrl}/group/join`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + jwtToken,
-      },
-      body: JSON.stringify(requestBody),
-    });
+    // const requestBody = {
+    //   inviteCode: inviteCode,
+    // };
 
-    if (response.ok) {
-      navigate("/auth");
-    } else {
-      // Handle the error
-    }
+    // const response = await fetch(`${apiUrl}/group/join`, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: "Bearer " + jwtToken,
+    //   },
+    //   body: JSON.stringify(requestBody),
+    // });
+
+    // if (response.ok) {
+    //   navigate("/auth");
+    // } else {
+    //   // Handle the error
+    // }
   };
 
   return (
@@ -88,108 +99,3 @@ function Guest() {
     </div>
   );
 }
-
-export default Guest;
-
-// import React, { useState } from 'react';
-// import './guest.css'
-
-// function Guest() {
-//   const [action, setAction] = useState(null);
-//   const [groupName, setGroupName] = useState('');
-//   const [inviteCode, setInviteCode] = useState('');
-
-//   const jwtToken = localStorage.getItem('jwtToken');
-
-//   const handleCreateGroup = () => {
-//     const requestBody = {
-//       name: groupName
-//     };
-
-//     fetch('http://localhost:8080/api/group/create', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer ' + jwtToken
-//       },
-//       body: JSON.stringify(requestBody)
-//     })
-//       .then(response => {
-//         if (response.headers.get('content-length') === '0' || !response.ok) {
-//           throw new Error('Empty response or response not OK');
-//         }
-//         return response.json();
-//       })
-//       .then(data => {
-//         console.log('Group created:', data);
-//         // Handle the response or show a success message
-//       })
-//       .catch(error => {
-//         console.error('Error:', error);
-//         // Handle the error or show a message to the user
-//       });
-//   };
-
-//   const handleJoinGroup = () => {
-//     const requestBody = {
-//       inviteCode: inviteCode
-//     };
-
-//     fetch('http://localhost:8080/api/group/join', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer ' + jwtToken
-//       },
-//       body: JSON.stringify(requestBody)
-//     })
-//       .then(response => {
-//         if (response.headers.get('content-length') === '0' || !response.ok) {
-//           throw new Error('Empty response or response not OK');
-//         }
-//         return response.json();
-//       })
-//       .then(data => {
-//         console.log('Joined group:', data);
-//         // Handle the response or show a success message
-//       })
-//       .catch(error => {
-//         console.error('Error:', error);
-//         // Handle the error or show a message to the user
-//       });
-//   };
-
-//   return (
-//     <div classNameName="main">
-//       <h2>Кімната очікування</h2>
-//       <button onClick={() => setAction('create')}>Створити групу</button>
-//       <button onClick={() => setAction('join')}>Приєднатись до групи</button>
-
-//       {action === 'create' && (
-//         <div>
-//           <label>Виберіть групу:</label>
-//           <select onChange={e => setGroupName(e.target.value)}>
-//             <option value="КБІКС-21-1">КБІКС-21-1</option>
-//             <option value="КБІКС-21-2">КБІКС-21-2</option>
-//             {/* You can add other group options here */}
-//           </select>
-//           <button onClick={handleCreateGroup}>Створити</button>
-//         </div>
-//       )}
-
-//       {action === 'join' && (
-//         <div>
-//           <label>Введіть код групи:</label>
-//           <input
-//             type="text"
-//             value={inviteCode}
-//             onChange={e => setInviteCode(e.target.value)}
-//           />
-//           <button onClick={handleJoinGroup}>Приєднатись</button>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Guest;
